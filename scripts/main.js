45d91bf67b42f0bb373a60e8ec9bc9b1d03e96ef
Severin Schols <severin@schols.de>
2011-03-13 16:09:21 +0100

    Loading and saving pages now working

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,50 @@
-//var page_text = "Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum";
+function openPage(page_title) {	
+	var tiddlyIO = new TiddlyIO();
+	var localPath = tiddlyIO.localPath;
+	var pagePath;
+	if((p = localPath.lastIndexOf("/")) != -1)
+		pagePath = localPath.substr(0,p) + "/";
+	else if((p = localPath.lastIndexOf("\\")) != -1)
+		pagePath = localPath.substr(0,p) + "\\";
+	else
+		pagePath = localPath + ".";
+	pagePath += "data\\" + page_title + ".txt";
+	var output = tiddlyIO.loadFile(pagePath);
+	return output;
+}
+
+function savePage(page_title, content) {
+	var tiddlyIO = new TiddlyIO();
+	var localPath = tiddlyIO.localPath;
+	var pagePath;
+	if((p = localPath.lastIndexOf("/")) != -1)
+		pagePath = localPath.substr(0,p) + "/";
+	else if((p = localPath.lastIndexOf("\\")) != -1)
+		pagePath = localPath.substr(0,p) + "\\";
+	else
+		pagePath = localPath + ".";
+	pagePath += "data\\" + page_title + ".txt";
+	var output = tiddlyIO.saveFile(pagePath, content);
+	return output;
+}
+
+
 
 /* jQuery textarea resizer plugin usage */
-  $(document).ready(function() {
-  	//document.getElementById('page_content').innerHtml = "Ketchup!";
+  $(document).ready(function() {  	
     $('textarea.resizable:not(.processed)').TextAreaResizer();
-    //$('#page_content').innerHtml = "mAJO";
-    
+	var page_text = openPage("loremipsum");
+    $('#page_content').html(page_text);
+    $('#page_content_edit').html(page_text);
+	$('#page_content_edit_save').click(function(event) {
+		event.preventDefault();
+		var r = savePage("loremipsum", $('#page_content_edit').val());
+		if (r) {
+			alert("Page saved!");
+			 $('#page_content').html( $('#page_content_edit').val());
+		} else {
+			alert("Error while saving");
+		}
+		
+	});
   });
